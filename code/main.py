ba6b96dd1a19f98f6036918df740f6edf079e64d
biglulu3310 <fz0935314576@gmail.com>
2025-02-21 13:00:25 +0800

    Noise MLE v1

---
+++ code/main.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 15:02:35 2025
+
+@author: cdrg
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+
+from nelder_mead import nelder_mead, log_likelihood
+
+#==================================================================
+noise_dir = "../data/"
+file_list = os.listdir(noise_dir)
+
+output_dir = "../output/"
+
+#==================================================================
+def import_data(_dir, filename):
+    data = []
+    with open(_dir + filename) as file:
+        for line in file:
+            data.append(line.strip().split())
+            
+    return data
+
+#==================================================================
+for st in file_list:
+    name, ext = st.split('.')
+    os.makedirs(output_dir, exist_ok=True)
+    
+    if os.path.exists(noise_dir + st):
+        [time, color, white] = np.array(import_data(noise_dir, st)[2::], 
+                                        dtype=float).T
+        
+        miss_ind = np.isnan(white)
+        
+        time = time[~miss_ind]
+        color = color[~miss_ind]
+        
+        dt = np.array([1/365.25]+[time[i+1]-time[i] 
+                                  for i in range(len(time)-1)],dtype=float)
+        
+        result = nelder_mead(log_likelihood, np.array([2, -1.2]), color, dt)
+        
+        amp = round(result[0][0], 2)
+        kappa = round(result[0][1], 2)
+        
+        #==========================================
+        fig0 = plt.figure(figsize=(18, 9))
+        ax0 = plt.subplot2grid((1, 1), (0, 0))
+            
+        ax0.plot(time, color, c='k', lw=0.8)
+        ax0.plot(time, color, 'o', c='k', ms=6.5)
+        
+        ax0.xaxis.set_major_locator(MultipleLocator(1))
+        
+        ax0.yaxis.set_minor_locator(AutoMinorLocator(5))  
+        ax0.xaxis.set_minor_locator(AutoMinorLocator(12))
+        
+        ax0.tick_params(axis='both', which='major',direction='in',
+                                bottom=True,top=True,right=True,left=True,
+                                length=15, width=3.1, labelsize=22,pad=12)
+        
+        ax0.tick_params(axis='both', which='minor',direction='in',
+                        bottom=True,top=True,right=True,left=True,
+                        length=9, width=2.5, labelsize=22,pad=12)
+        
+        [ax0.spines[b].set_linewidth(2.6) for b in ['top', 'bottom','left','right']]
+        [ax0.spines[b].set_color("black") for b in ['top', 'bottom','left','right']]
+        
+        ax0.set_title("spectral index($\kappa$): {0}\n \
+                       colored noise amplitude: {1}".format(kappa, amp), 
+                       fontsize=18, pad=20, ha='right', x=0.985, linespacing=1.6) 
+        
+        ax0.set_xlabel("time in year", fontsize=28, labelpad=15)
+        ax0.set_ylabel("colored noise (mm)", fontsize=28, labelpad=15)
+        
+        fig0.suptitle("{0} Colored Noise".format(name), fontsize=45, y=0.91)
+        plt.tight_layout()
+        plt.savefig(output_dir + name + ".png", dpi=400)
+        
+        
+        
---
+++ code/nelder_mead.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 11:54:55 2025
+
+@author: cdrg
+"""
+
+from numba import jit
+import numpy as np
+
+#===============================================
+@jit(nopython=True, cache=True)
+def create_C(sigma_pl, kappa, dt):
+    N = len(dt)
+    U = np.eye(N)
+    h = np.ones(N)
+    
+    coeff = 1 + kappa/2
+    for i in range(1, N):
+       h[i] = h[i-1] * (i - coeff) / i 
+    
+    for i in range(1, N):
+        for j in range(0, N - i):
+            U[j, j + i] = h[i]
+       
+    scale_factors = sigma_pl * (dt ** (-kappa / 4))
+    for j in range(N):
+        U[j, :] *= scale_factors[j]
+    
+    return np.dot(U.T, U)
+
+
+#===============================================
+@jit(nopython=True, cache=True)
+def log_likelihood(theta, noise, dt):
+    # thata[0]: amp, theta[1]: kappa
+    N = len(noise)
+    C = create_C(theta[0], theta[1], dt)
+    r = noise.ravel()
+    
+    U = np.linalg.cholesky(C).T
+    
+    ln_det_C = 0.0
+    for i in range(0, N):
+        ln_det_C += 2 * np.log(U[i, i])
+    
+    U_inv_r = np.linalg.solve(U, r)
+    r_T_C_inv_r = np.dot(U_inv_r.T, U_inv_r)
+    
+    n_logL = ln_det_C + r_T_C_inv_r
+    
+    return n_logL
+
+
+#===============================================
+@jit(nopython=True)
+def nelder_mead(func, x, noise, dt, step=0.1, tol=1e-2, max_itr=50, 
+                alpha=1., gamma=2., rho=0.5, sigma=0.5):
+    n = len(x)
+    simplex = np.zeros((n+1, n))
+    f = np.zeros(n+1)
+    
+    #===============================================
+    # init simplex
+    simplex[0] = x
+    f[0] = func(x, noise, dt)
+    
+    for i in range(1, n+1):
+        simplex[i] = x
+        simplex[i, i-1] += step
+        
+        f[i] = func(simplex[i], noise, dt)
+    
+    #===============================================
+    # optimization
+    itrs = 0
+    while True:
+        itrs += 1
+        # sort
+        sort_ind = f.argsort()
+        simplex = simplex[sort_ind]
+        f = f[sort_ind]
+        
+        #===========================
+        """--Termination--"""
+        # max iteration
+        if max_itr and itrs >= max_itr:
+            print("itrs: " + str(itrs))
+            print("Terminated due to reaching max iterations: max_itr=" + str(max_itr) + " !")
+            return simplex[0], f[0]
+        
+        # tol
+        if np.max(np.abs(f - f[0])) < tol:
+            print("itrs: " + str(itrs))
+            print("Terminated due to tolerance!")
+            return simplex[0], f[0]
+        
+        #===========================
+        # centroid of x_1:n
+        x_bar = simplex[:-1].sum(axis=0) / n
+        
+        #===========================
+        # reflection
+        x_r = x_bar + alpha * (x_bar - simplex[-1])
+        f_r = func(x_r, noise, dt)
+        
+        #=============================================
+        '''--main--'''
+        # reflection: between best and sub_worst
+        if f[0] <= f_r < f[-2]:
+            simplex[-1] = x_r
+            f[-1] = f_r
+            
+        # reflection: best
+        elif f_r < f[0]:
+            x_e = x_bar + gamma * (x_r - x_bar)
+            f_e = func(x_e, noise, dt)
+            
+            if f_e < f_r:
+                simplex[-1] = x_e
+                f[-1] = f_e
+            
+            else:
+                simplex[-1] = x_r
+                f[-1] = f_r
+            
+        # reflection: worse than sub_worst
+        else:
+            x_c = x_bar + rho * (simplex[-1] - x_bar)
+            f_c = func(x_c, noise, dt)
+            
+            if f_c < f[-1]:
+                simplex[-1] = x_c
+                f[-1] = f_c
+            
+            else:
+                # shrink
+                for i in range(1, n+1):
+                    simplex[i] = simplex[0] + sigma * (simplex[i] - simplex[0])
+                    f[i] = func(simplex[i], noise, dt)
+                    
